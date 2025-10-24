f2910ed274f8aae2226a18b9e4360c60194f6a07
jonapano <panojona1@gmail.com>
2025-10-24 15:44:48 +0200

    testt

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  experimental: {
+    turbo: {
+      rules: {}, // empty instead of false
+    },
+  },
+  webpack: (config) => {
+    return config; // ensures Webpack is used
+  },
 };
 
 export default nextConfig;
-
